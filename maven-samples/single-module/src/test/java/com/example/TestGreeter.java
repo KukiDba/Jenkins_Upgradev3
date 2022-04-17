9c0ef5bc3f6685a90e8acb47612ac836b1431aa8
unknown <bharatdevopsdba@gmail.com>
2022-04-17 19:02:18 +0530

    my first commit 11

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Bharat Singh Bisht";
+    String someone = "World, Thanks you making me Yuwantsh";
 
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
